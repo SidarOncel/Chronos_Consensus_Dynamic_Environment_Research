b5888d3a62f62f11f58a62352e14dc0c9ac6c2f4
Sidar Oncel <oncelsidar@gmail.com>
2026-05-29 23:49:55 +0800

    Deleted comments, put the defined vars to common types instead of putting other header files like hal.h

--- core/common_types.h
+++ core/common_types.h
@@ -5,83 +5,6 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-
-/*********************************************************************
- * Runtime node state
- *********************************************************************/
-
-typedef struct {
-
-    uint8_t node_id;
-
-    RaftState_t state;
-
-    uint32_t current_term;
-
-    uint8_t voted_for;
-
-    uint8_t leader_id;
-
-    uint32_t commit_index;
-
-    uint32_t last_applied;
-
-} NodeRuntimeState_t;
-
-/*********************************************************************
- * Election statistics
- *
- * These metrics are logged during experiments.
- *********************************************************************/
-
-typedef struct {
-
-    uint32_t election_count;
-
-    uint32_t successful_elections;
-
-    uint32_t failed_elections;
-
-    uint64_t average_election_time_ms;
-
-    uint64_t longest_election_time_ms;
-
-} ElectionStatistics_t;
-
-/*********************************************************************
- * Experimental network impairment model
- *
- * Used to emulate dynamic wireless conditions.
- *********************************************************************/
-
-typedef struct {
-
-    float packet_loss_pct;
-
-    uint32_t fixed_delay_ms;
-
-    uint32_t jitter_ms;
-
-    float doppler_shift_hz;
-
-    int8_t simulated_rssi;
-
-} NetworkImpairment_t;
-
-/*********************************************************************
- * RAFT timing configuration
- *********************************************************************/
-
-#define RAFT_HEARTBEAT_INTERVAL_MS   50
-
-/*
- * Randomized election timeout range.
- *
- * Randomization prevents split votes.
- */
-#define RAFT_ELECTION_TIMEOUT_MIN_MS 150
-#define RAFT_ELECTION_TIMEOUT_MAX_MS 300
-
 /*********************************************************************
  * Generic constants
  *********************************************************************/
@@ -93,6 +16,9 @@ typedef struct {
 #define RAFT_MAX_LOG_ENTRIES   128
 #define RAFT_MAX_NODES         8
 
+#define RAFT_HEARTBEAT_INTERVAL_MS   50
+#define RAFT_ELECTION_TIMEOUT_MIN_MS 150
+#define RAFT_ELECTION_TIMEOUT_MAX_MS 300
 
 /*********************************************************************
  * Generic result codes
@@ -113,269 +39,36 @@ typedef enum {
     CHRONOS_ERR_NOT_FOUND   = -4,
     CHRONOS_ERR_COMM_FAIL   = -5,
     CHRONOS_ERR_CONFLICT    = -6
-
 } ChronosResult_t;
-
 
 /*********************************************************************
  * RAFT message types
- *
- * These represent the logical packet purpose.
  *********************************************************************/
 
 typedef enum {
-
     MSG_NONE = 0,
 
-    /*
-     * Leader election
-     */
     MSG_REQUEST_VOTE,
     MSG_VOTE_RESPONSE,
 
-    /*
-     * Heartbeats + replication
-     */
     MSG_APPEND_ENTRIES,
     MSG_APPEND_RESPONSE,
 
-    /*
-     * Experiment/debug traffic
-     */
     MSG_STATUS,
     MSG_EXPERIMENT_DATA
-
 } MessageType_t;
-
 
 /*********************************************************************
  * RAFT roles / states
  *********************************************************************/
 
 typedef enum {
-
     RAFT_FOLLOWER = 0,
     RAFT_CANDIDATE,
     RAFT_LEADER
-
 } RaftState_t;
 
-
-/*********************************************************************
- * RAFT log entry types
- *********************************************************************/
-
-typedef enum {
-
-    RAFT_ENTRY_TASK_ASSIGN = 0,
-    RAFT_ENTRY_HEARTBEAT,
-    RAFT_ENTRY_DEMO
-
-} RaftEntryType_t;
-
-
-/*********************************************************************
- * Generic packet header
- *
- * Every UDP packet begins with this structure.
- *********************************************************************/
-
-typedef struct {
-
-    uint8_t  magic[4];
-
-    uint16_t version;
-
-    uint16_t type;
-
-    uint32_t term;
-
-    uint32_t seq_num;
-
-    uint8_t  src_id;
-
-    uint8_t  dst_id;
-
-    uint32_t payload_len;
-
-    uint32_t crc32;
-
-} MessageHeader_t;
-
-
-/*********************************************************************
- * RAFT vote request
- *
- * Used by candidate during election to request votes from peers.
- *********************************************************************/
-
-typedef struct {
-
-    uint32_t term;
-
-    uint8_t candidate_id;
-
-    uint32_t last_log_index;
-
-    uint32_t last_log_term;
-
-} VoteRequest_t;
-
-
-/*********************************************************************
- * RAFT vote response
- *
- * Follower responds with this after receiving a vote request.
- *********************************************************************/
-
-typedef struct {
-
-    uint32_t term;
-
-    uint8_t vote_granted;
-
-} VoteResponse_t;
-
-
-/*********************************************************************
- * RAFT append entries request
- *
- * Used by leader for heartbeats and log replication.
- *********************************************************************/
-
-typedef struct {
-
-    uint32_t term;
-
-    uint8_t leader_id;
-
-    uint32_t prev_log_index;
-
-    uint32_t prev_log_term;
-
-    uint32_t leader_commit;
-
-    uint32_t entry_len;
-
-} AppendEntriesRequest_t;
-
-
-/*********************************************************************
- * RAFT append entries response
- *
- * Follower responds with this after append entries.
- *********************************************************************/
-
-typedef struct {
-
-    uint32_t term;
-
-    uint8_t success;
-
-    uint32_t match_index;
-
-} AppendEntriesResponse_t;
-
-typedef struct {
-    uint32_t term;
-    uint32_t index;
-    RaftEntryType_t type;
-    uint32_t data_len;
-    uint8_t  committed;
-    char data[128];
-} RaftLogEntry_t;
-
-
-/*********************************************************************
- * Node information
- *********************************************************************/
-
-typedef struct {
-
-    uint8_t node_id;
-
-    uint32_t ip_addr;
-
-    uint16_t port;
-
-    int8_t rssi;
-
-    uint64_t last_seen;
-
-    uint8_t is_alive;
-
-} NodeInfo_t;
-
-
-/*********************************************************************
- * Cluster state
- *********************************************************************/
-
-typedef struct {
-
-    uint8_t total_nodes;
-
-    uint8_t current_leader;
-
-    uint32_t current_term;
-
-} ClusterInfo_t;
-
-
-/*********************************************************************
- * Communication statistics
- *
- * These become paper metrics later.
- *********************************************************************/
-
-typedef struct {
-
-    uint64_t packets_sent;
-
-    uint64_t packets_received;
-
-    uint64_t bytes_sent;
-
-    uint64_t bytes_received;
-
-    uint64_t send_errors;
-
-    uint64_t receive_errors;
-
-    uint64_t crc_errors;
-
-    int8_t min_rssi;
-
-    int8_t max_rssi;
-
-} CommStatistics_t;
-
-
-/*********************************************************************
- * State change callback prototype
- *
- * Called whenever the RAFT node transitions between states.
- * Used by main.c to log state transitions and measure election latency.
- *********************************************************************/
-
-typedef void (*StateChangeCallback_t)(
-    RaftState_t old_state,
-    RaftState_t new_state
-);
-
-
-/*********************************************************************
- * Message callback prototype
- *
- * The network layer calls these when packets arrive.
- *********************************************************************/
-
-typedef void (*MessageCallback_t)(
-    const MessageHeader_t* header,
-    const void* payload,
-    size_t payload_len
-);
-
+/* Compatibility aliases used by the current source files */
 #define RAFT_STATE_FOLLOWER   RAFT_FOLLOWER
 #define RAFT_STATE_CANDIDATE  RAFT_CANDIDATE
 #define RAFT_STATE_LEADER     RAFT_LEADER
@@ -385,5 +78,170 @@ typedef void (*MessageCallback_t)(
 #define MSG_TYPE_APPEND_REQ   MSG_APPEND_ENTRIES
 #define MSG_TYPE_APPEND_RES   MSG_APPEND_RESPONSE
 
+/*********************************************************************
+ * RAFT log entry types
+ *********************************************************************/
+
+typedef enum {
+    RAFT_ENTRY_TASK_ASSIGN = 0,
+    RAFT_ENTRY_HEARTBEAT,
+    RAFT_ENTRY_DEMO
+} RaftEntryType_t;
+
+/*********************************************************************
+ * Runtime node state
+ *********************************************************************/
+
+typedef struct {
+    uint8_t node_id;
+    RaftState_t state;
+    uint32_t current_term;
+    uint8_t voted_for;
+    uint8_t leader_id;
+    uint32_t commit_index;
+    uint32_t last_applied;
+} NodeRuntimeState_t;
+
+/*********************************************************************
+ * Election statistics
+ *********************************************************************/
+
+typedef struct {
+    uint32_t election_count;
+    uint32_t successful_elections;
+    uint32_t failed_elections;
+    uint64_t average_election_time_ms;
+    uint64_t longest_election_time_ms;
+} ElectionStatistics_t;
+
+/*********************************************************************
+ * Experimental network impairment model
+ *********************************************************************/
+
+typedef struct {
+    float packet_loss_pct;
+    uint32_t fixed_delay_ms;
+    uint32_t jitter_ms;
+    float doppler_shift_hz;
+    int8_t simulated_rssi;
+} NetworkImpairment_t;
+
+/*********************************************************************
+ * Generic packet header
+ *********************************************************************/
+
+typedef struct {
+    uint8_t  magic[4];
+    uint16_t version;
+    uint16_t type;
+    uint32_t term;
+    uint32_t seq_num;
+    uint8_t  src_id;
+    uint8_t  dst_id;
+    uint32_t payload_len;
+    uint32_t crc32;
+} MessageHeader_t;
+
+/*********************************************************************
+ * RAFT vote request / response
+ *********************************************************************/
+
+typedef struct {
+    uint32_t term;
+    uint8_t candidate_id;
+    uint32_t last_log_index;
+    uint32_t last_log_term;
+} VoteRequest_t;
+
+typedef struct {
+    uint32_t term;
+    uint8_t vote_granted;
+} VoteResponse_t;
+
+/*********************************************************************
+ * RAFT append entries request / response
+ *********************************************************************/
+
+typedef struct {
+    uint32_t term;
+    uint8_t leader_id;
+    uint32_t prev_log_index;
+    uint32_t prev_log_term;
+    uint32_t leader_commit;
+    uint32_t entry_len;
+} AppendEntriesRequest_t;
+
+typedef struct {
+    uint32_t term;
+    uint8_t success;
+    uint32_t match_index;
+} AppendEntriesResponse_t;
+
+/*********************************************************************
+ * RAFT log entry
+ *********************************************************************/
+
+typedef struct {
+    uint32_t term;
+    uint32_t index;
+    RaftEntryType_t type;
+    uint32_t data_len;
+    uint8_t committed;
+    char data[128];
+} RaftLogEntry_t;
+
+/*********************************************************************
+ * Node information
+ *********************************************************************/
+
+typedef struct {
+    uint8_t node_id;
+    uint32_t ip_addr;
+    uint16_t port;
+    int8_t rssi;
+    uint64_t last_seen;
+    uint8_t is_alive;
+} NodeInfo_t;
+
+/*********************************************************************
+ * Cluster state
+ *********************************************************************/
+
+typedef struct {
+    uint8_t total_nodes;
+    uint8_t current_leader;
+    uint32_t current_term;
+} ClusterInfo_t;
+
+/*********************************************************************
+ * Communication statistics
+ *********************************************************************/
+
+typedef struct {
+    uint64_t packets_sent;
+    uint64_t packets_received;
+    uint64_t bytes_sent;
+    uint64_t bytes_received;
+    uint64_t send_errors;
+    uint64_t receive_errors;
+    uint64_t crc_errors;
+    int8_t min_rssi;
+    int8_t max_rssi;
+} CommStatistics_t;
+
+/*********************************************************************
+ * Callback prototypes
+ *********************************************************************/
+
+typedef void (*StateChangeCallback_t)(
+    RaftState_t old_state,
+    RaftState_t new_state
+);
+
+typedef void (*MessageCallback_t)(
+    const MessageHeader_t* header,
+    const void* payload,
+    size_t payload_len
+);
 
 #endif
--- core/hal.h
+++ core/hal.h
@@ -3,147 +3,31 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdarg.h>
+
+#include "common_types.h"
 
 /*
  * HAL = Hardware Abstraction Layer
  *
  * This file separates the consensus logic from platform-specific details.
- *
- * Why this matters:
- * -----------------
- * RAFT itself should not care whether it runs on:
- *   - a Windows laptop,
- *   - Linux PC,
- *   - Milk-V Duo,
- *   - or another RISC-V board.
- *
- * The consensus layer only asks:
- *   - "What time is it?"
- *   - "Wait a little."
- *   - "Print this message."
- *
- * HAL is responsible for answering those requests using whatever operating
- * system or hardware is available underneath.
- *
- * Think of HAL like a translator between:
- *   consensus logic  <->  operating system / hardware
+ * RAFT should not care whether it runs on a Windows laptop, Linux PC,
+ * Milk-V Duo, or another RISC-V board.
  */
 
-/* -------------------------------------------------------------------------
- * Generic result codes
- * -------------------------------------------------------------------------
- *
- * We keep the result enum very small in the paper repo.
- * The large AGV project may contain many more detailed error types.
- */
-typedef enum {
-    CHRONOS_OK = 0,
-    CHRONOS_ERR_INVALID_ARG = -1,
-    CHRONOS_ERR_TIMEOUT = -2,
-    CHRONOS_ERR_COMM_FAIL = -3,
-    CHRONOS_ERR_NO_MEMORY = -4,
-    CHRONOS_ERR_INTERNAL = -5
-} ChronosResult_t;
-
-/* -------------------------------------------------------------------------
- * HAL lifecycle
- * -------------------------------------------------------------------------
- *
- * Example:
- *
- *   hal_init();
- *   ...
- *   hal_shutdown();
- *
- * On a PC this may initialize timers or sockets.
- * On an embedded board it could initialize clocks or peripherals.
- */
+/* HAL lifecycle */
 ChronosResult_t hal_init(void);
 ChronosResult_t hal_shutdown(void);
 
-/* -------------------------------------------------------------------------
- * Timing functions
- * -------------------------------------------------------------------------
- *
- * Timing is extremely important in RAFT.
- *
- * Leader election depends on:
- *   - heartbeat intervals
- *   - election timeouts
- *   - retry timing
- *
- * If timing becomes inconsistent, the cluster may:
- *   - trigger unnecessary elections,
- *   - oscillate between leaders,
- *   - or become unstable.
- *
- * That is why the timing layer is abstracted cleanly here.
- */
-
-/*
- * Return current time in milliseconds.
- *
- * Example:
- *   uint64_t now = hal_get_time_ms();
- */
+/* Timing */
 uint64_t hal_get_time_ms(void);
-
-/*
- * Return current time in microseconds.
- *
- * Useful for:
- *   - latency measurement,
- *   - profiling,
- *   - packet timing experiments.
- */
 uint64_t hal_get_time_us(void);
-
-/*
- * Delay execution for a number of milliseconds.
- *
- * Example:
- *   hal_delay_ms(10);
- *
- * In the paper repo we use small delays so the main loop:
- *   - does not consume 100% CPU,
- *   - behaves more predictably,
- *   - produces cleaner timing logs.
- */
 void hal_delay_ms(uint32_t delay_ms);
 
-/* -------------------------------------------------------------------------
- * Logging / console output
- * -------------------------------------------------------------------------
- *
- * Instead of scattering printf() everywhere,
- * we centralize logging through HAL.
- *
- * Why this is useful:
- *   - easier to redirect logs later,
- *   - easier to timestamp logs,
- *   - easier to disable logs on embedded targets,
- *   - cleaner experiment output.
- *
- * Example:
- *   hal_console_log("[RAFT] Node became leader");
- */
+/* Logging */
 void hal_console_log(const char* fmt, ...);
 
-/* -------------------------------------------------------------------------
- * Utility helper
- * -------------------------------------------------------------------------
- *
- * Sometimes experiments need a random timeout offset.
- *
- * Example:
- *   RAFT election timeout randomization
- *
- * Without randomization:
- *   every node might start elections at the same time,
- *   causing repeated split votes.
- *
- * Small random jitter improves cluster stability.
- */
+/* Utility */
 uint32_t hal_random_u32(void);
 
 #endif /* CHRONOS_HAL_H */
